af5380e9ab3762e66e40dded069681c881b38e9e
jnnjh <joannejhoieacar@gmail.com>
2026-06-06 08:52:33 +0200

    uploaded all qoute-generator files

---
+++ frontend/index.js
@@ -0,0 +1,21 @@
+const quoteElement = document.getElementById("quote");
+const authorElement = document.getElementById("author");
+const button = document.getElementById("new-quote-btn");
+
+async function fetchQuote() {
+  try {
+    const response = await fetch("http://localhost:3000");
+    const quote = await response.json();
+
+    quoteElement.textContent = `"${quote.quote}"`;
+    authorElement.textContent = `— ${quote.author}`;
+  } catch (error) {
+    quoteElement.textContent = "Failed to load quote.";
+    authorElement.textContent = "";
+    console.error(error);
+  }
+}
+
+button.addEventListener("click", fetchQuote);
+
+fetchQuote();
